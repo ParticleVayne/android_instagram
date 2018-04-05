df13c9021f4f0ff20cb913793c63bea7be799fc0
Jeric <jeric.delacruz07@gmail.com>
2018-04-05 09:52:51 +0800

    Initial commit

---
+++ app/src/main/java/com/delaroystudios/instagramintegration/example/ApplicationData.java
@@ -0,0 +1,7 @@
+package com.delaroystudios.instagramintegration.example;
+
+public class ApplicationData {
+	public static final String CLIENT_ID = "49745eedbd964008acc78f16ddab8c0f";
+	public static final String CLIENT_SECRET = "bed60a4f055e485da48508311de68feb";
+	public static final String CALLBACK_URL = "http://www.karlovigilia.com";
+}
